329567ec6190e829d8867f12fecb3acebca1bcb1
sotudehg <sotudehg>
2021-05-30 02:40:14 -0400

    pdb

--- src/prepro/data_builder.py
+++ src/prepro/data_builder.py
@@ -121,6 +121,7 @@ def tokenize(args):
                '-ssplit.newlineIsSentenceBreak', 'always', '-filelist', 'mapping_for_corenlp.txt', '-outputFormat',
                'json', '-outputDirectory', tokenized_stories_dir]
     print("Tokenizing %i files in %s and saving in %s..." % (len(stories), stories_dir, tokenized_stories_dir))
+    import pdb;pdb.set_trace()
     subprocess.call(command)
     print("Stanford CoreNLP Tokenizer has finished.")
     os.remove("mapping_for_corenlp.txt")
