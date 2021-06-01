1ea6659da939fd38a8000ac4ca12431246ad071b
sotudehg <sotudehg>
2021-06-01 09:31:04 -0400

    update prep

--- src/prep.sh
+++ src/prep.sh
@@ -5,5 +5,5 @@ export TOKENIZED_PATH=/home/code-base/user_space/datasets/reddit-tifu/tokenized-
 export JSON_PATH=/home/code-base/user_space/datasets/reddit-tifu/jsons/
 
 #python preprocess.py -mode tokenize -raw_path $RAW_PATH -save_path $TOKENIZED_PATH
-python preprocess.py -mode format_to_lines -raw_path $RAW_PATH -save_path $JSON_PATH -n_cpus 20 -use_bert_basic_tokenizer false -map_path MAP_PATH
+python preprocess.py -mode format_to_lines -raw_path $RAW_PATH -save_path $JSON_PATH -n_cpus 20 -use_bert_basic_tokenizer false
 
