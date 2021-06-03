1d582f3df703aeba53f34664a6255845e6b1a20e
sotudehg <sotudehg>
2021-06-03 10:58:47 -0400

    update prep

--- src/ext_train.sh
+++ src/ext_train.sh
@@ -8,5 +8,5 @@ python train.py -task ext -mode train \
   -bert_data_path $BERT_DATA_PATH -ext_dropout 0.1 -model_path $MODEL_PATH \
   -lr 2e-3 -visible_gpus 0,1,2,3 -report_every 50 -save_checkpoint_steps 2000 \
   -batch_size 3000 -train_steps 100000 -accum_count 2 \
-  -log_file ../logs/ext_bert_reddit -use_interval true -warmup_steps 10000 -max_pos 512 \
+  -log_file ../logs/ext_bert_reddit -use_interval true -warmup_steps 10000 -max_pos 1024 \
   -train_from /home/code-base/user_space/saved_models/bertExtCNN/bertext_cnndm_transformer.pt
