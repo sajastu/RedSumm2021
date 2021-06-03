75f63b02f3e7341bc20fe806fee935b80a47f75d
sotudehg <sotudehg>
2021-06-03 13:55:18 -0400

    update prep

--- src/models/model_builder.py
+++ src/models/model_builder.py
@@ -145,6 +145,7 @@ class Bert(nn.Module):
         else:
             self.eval()
             with torch.no_grad():
+                import pdb;pdb.set_trace()
                 top_vec = self.model(input_ids=x, token_type_ids=segs, attention_mask=mask)['last_hidden_state']
                 # top_vec, _ = self.model(x, token_type_ids=segs, attention_mask=mask)
 
