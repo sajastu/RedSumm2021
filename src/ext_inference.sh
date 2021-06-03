7ad151112b466f33f9afea92c7f8885ee1500815
sotudehg <sotudehg>
2021-06-02 21:03:57 -0400

    update prep

--- src/ext_inference.sh
+++ src/ext_inference.sh
@@ -3,12 +3,12 @@ BERT_DATA_PATH=/home/code-base/user_space/datasets/reddit-tifu/bert-data/
 MODEL_PATH=/home/code-base/user_space/saved_models/bertsum/presum-reddit-1024/
 
  python train.py -task ext -mode validate \
-                  -batch_size 3000 -test_batch_size 500 \
+                  -batch_size 3000 -test_batch_size 1000 \
                   -bert_data_path $BERT_DATA_PATH \
                   -log_file ../logs/val_abs_bert_reddit \
                   -model_path $MODEL_PATH \
                   -sep_optim true -use_interval true \
-                  -visible_gpus 0,1 -max_pos 1024 -max_length 500 -alpha 0.95 \
+                  -visible_gpus 0,1,2,3 -max_pos 1024 -max_length 500 -alpha 0.95 \
                   -min_length 50 \
                   -result_path $MODEL_PATH/results/ \
                   -large
