638b2d308377230c2b0ee2881ca3770ab96f8c02
sotudehg <sotudehg>
2021-06-03 12:25:09 -0400

    update prep

--- src/abs_test.sh
+++ src/abs_test.sh
@@ -4,11 +4,11 @@ MODEL_PATH=/home/code-base/user_space/saved_models/bertsum/presum-reddit-1024-Be
 
 
  python train.py -task abs -mode validate \
-                -batch_size 3000 -test_batch_size 200 \
+                -batch_size 3000 -test_batch_size 100 \
                 -bert_data_path $BERT_DATA_PATH \
                 -log_file ../logs/val_abs_bert_cnndm \
                 -model_path $MODEL_PATH -sep_optim true \
-                -use_interval true -visible_gpus 1 \
+                -use_interval true -visible_gpus 0,1,2,3 \
                 -max_pos 1024 -max_length 128 \
                 -alpha 0.95 -min_length 20 \
                 -result_path ../logs/abs_bert_cnndm
