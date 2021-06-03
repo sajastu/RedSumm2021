74a5034edbf0f3e49637a689a5a4ad9458d92b80
sotudehg <sotudehg>
2021-06-03 13:09:46 -0400

    update prep

--- src/train_abstractive.py
+++ src/train_abstractive.py
@@ -140,7 +140,7 @@ def validate_abs(args, device_id):
             test_abs(args, device_id, cp, step)
     else:
         while (True):
-            cp_files = sorted(glob.glob(os.path.join(args.model_path, 'model_step_*.pt')))
+            cp_files = sorted(glob.glob(os.path.join(args.model_path, 'model_step_*.pt')))[:2]
             cp_files.sort(key=os.path.getmtime)
             if (cp_files):
                 cp = cp_files[-1]
