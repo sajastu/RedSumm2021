cd36db447bda251a426caa2090bbd955f6f988a1
sotudehg <sotudehg>
2021-06-04 16:21:41 -0400

    update prep

--- src/models/model_builder.py
+++ src/models/model_builder.py
@@ -198,7 +198,6 @@ class AbsSummarizer(nn.Module):
         self.bert = Bert(args.large, args.temp_dir, args=args, finetune=args.finetune_bert)
 
         if bert_from_extractive is not None:
-            import pdb;pdb.set_trace()
             self.bert.model.load_state_dict(dict([(n[11:], p) for n, p in bert_from_extractive.items() if n.startswith('bert.model')]), strict=True)
 
         if (args.encoder == 'baseline'):
