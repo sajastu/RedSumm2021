48ab1f11908eca06d54505453aed196b5f57c81f
sotudehg <sotudehg>
2021-06-04 17:18:19 -0400

    update prep

--- src/train_bertExtAbs.sh
+++ src/train_bertExtAbs.sh
@@ -14,7 +14,7 @@ python train.py  -task abs -mode train \
                  -model_path $MODEL_PATH \
                  -sep_optim true -lr_bert 0.002 -lr_dec 0.2 \
                  -save_checkpoint_steps 2000 \
-                 -batch_size 40 -train_steps 200000 -report_every 50 \
+                 -batch_size 20 -train_steps 200000 -report_every 50 \
                  -accum_count 5 -use_bert_emb true -use_interval true \
                  -warmup_steps_bert 20000 -warmup_steps_dec 10000 \
                  -max_pos 1024 -visible_gpus 0,1,2,3,4 \
