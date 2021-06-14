8af0b0bf73666bdd7ecbbcedf8eea9f1e9914bc1
sotudehg <sotudehg>
2021-06-14 14:15:17 -0400

    update prep

--- reddit_to_json.py
+++ reddit_to_json.py
@@ -4,11 +4,17 @@ BASE_DIR = '/home/code-base/user_space/packages/summarization_datasets/datasets/
 
 for set in ['train', 'val', 'test']:
     i = 0
-    json_file = open(f'{BASE_DIR}/{set}.json', mode='a')
+    # json_file = open(f'{BASE_DIR}/{set}.json', mode='a')
+    srcs = []
+    tgts = []
     with open(f'{BASE_DIR}/{set}.source') as fS, open(
             f'{BASE_DIR}/{set}.target') as fT:
         for s, t in zip(fS, fT):
-            json.dump(
-                {'id': f'{set}-{i}', 'text': s.replace('\n', ' ').strip(), 'summary': t.replace('\n', ' ').strip()},
-                json_file)
-            json_file.write('\n')
+            srcs.append(s.replace('\n', ' ').strip())
+            tgts.append(t.replace('\n', ' ').strip())
+
+        import pdb;pdb.set_trace()
+            # json.dump(
+            #     {'id': f'{set}-{i}', 'text': s.replace('\n', ' ').strip(), 'summary': t.replace('\n', ' ').strip()},
+            #     json_file)
+            # json_file.write('\n')
