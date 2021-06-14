c7baf5665e36e1bb24e81cec2cfb76663c467b99
sotudehg <sotudehg>
2021-06-14 14:06:54 -0400

    update prep

--- reddit_to_json.py
+++ reddit_to_json.py
@@ -1,10 +1,12 @@
 import json
 
+BASE_DIR = '/home/code-base/user_space/packages/summarization_datasets/datasets/'
+
 for set in ['train', 'validation', 'test']:
     i = 0
-    json_file = open(f'/home/code-base/user_space/datasets/reddit-tifu/splits-pegasus/reddit_tifu/{set}.json', mode='a')
-    with open(f'/home/code-base/user_space/datasets/reddit-tifu/splits-pegasus/reddit_tifu/{set}.source') as fS, open(
-            f'/home/code-base/user_space/datasets/reddit-tifu/splits-pegasus/reddit_tifu/{set}.target') as fT:
+    json_file = open(f'{BASE_DIR}/{set}.json', mode='a')
+    with open(f'{BASE_DIR}/{set}.source') as fS, open(
+            f'{BASE_DIR}/{set}.target') as fT:
         for s, t in zip(fS, fT):
             json.dump({'id': f'{set}-{i}', 'text': s.strip(), 'summary': t.strip()}, json_file)
             json_file.write('\n')
