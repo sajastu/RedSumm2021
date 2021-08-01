dd3bc02879c96d9fa733b31c7694c774bb82ea5f
sotudehg <sotudehg>
2021-08-01 05:12:28 -0400

    init

--- src/models/trainer_ext.py
+++ src/models/trainer_ext.py
@@ -350,9 +350,6 @@ class Trainer(object):
             sent_scores, mask = self.model(src, segs, clss, mask, mask_cls)
 
             loss = self.loss(sent_scores, labels.float())
-            import pdb;
-            pdb.set_trace()
-
             loss = (loss * mask.float()).sum()
 
             (loss / loss.numel()).backward()
