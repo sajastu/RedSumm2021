f55a5a5d058f320d4933abb67dc0fdfb1500a228
sotudehg <sotudehg>
2021-08-04 18:47:32 -0400

    init

--- src/ds_large/run_tokenization_onSensei.bash
+++ src/ds_large/run_tokenization_onSensei.bash
@@ -10,11 +10,11 @@ id=m_0
 #file_id=1Xzr3ZUbWLTcxFPUdTAsKwsL_T0PHLSHk #my_machine
 
 #pip install gdown
-#current=pwd
-#mkdir -p /home/code-base/mashine_split_lrg_reddit/
-#cd /home/code-base/mashine_split_lrg_reddit/
+current=pwd
+mkdir -p /home/code-base/mashine_split_lrg_reddit/
+cd /home/code-base/mashine_split_lrg_reddit/
 ## fill_out id for $id
-#gdown --id $file_id /home/code-base/mashine_split_lrg_reddit/
+gdown --id $file_id /home/code-base/mashine_split_lrg_reddit/
 #tar -xf /home/code-base/mashine_split_lrg_reddit/$id.tar
 #
 #cd $current
@@ -38,15 +38,15 @@ id=m_0
 #mkdir -p /home/code-base/lrg_split_s/$id/jsons
 #mkdir -p /home/code-base/lrg_split_s/$id/bert-data
 
-DS_BASE_DIR=/home/code-base/lrg_split_s/$id/
-export RAW_PATH=$DS_BASE_DIR/splits/
-export TOKENIZED_PATH=$DS_BASE_DIR/tokenized/
-export JSON_PATH=$DS_BASE_DIR/jsons/
-export BERT_DATA_PATH=$DS_BASE_DIR/bert-data/
-cd ../
-
-# PREPARING DATA
-python preprocess.py -mode tokenize -raw_path $RAW_PATH -save_path $TOKENIZED_PATH
-python preprocess.py -mode format_to_lines -raw_path $TOKENIZED_PATH -save_path $JSON_PATH -n_cpus 64 -use_bert_basic_tokenizer false
-python preprocess.py -mode format_to_bert -raw_path $JSON_PATH -save_path $BERT_DATA_PATH  -lower -n_cpus 64 -log_file ../logs/preprocess.log
+#DS_BASE_DIR=/home/code-base/lrg_split_s/$id/
+#export RAW_PATH=$DS_BASE_DIR/splits/
+#export TOKENIZED_PATH=$DS_BASE_DIR/tokenized/
+#export JSON_PATH=$DS_BASE_DIR/jsons/
+#export BERT_DATA_PATH=$DS_BASE_DIR/bert-data/
+#cd ../
+#
+## PREPARING DATA
+#python preprocess.py -mode tokenize -raw_path $RAW_PATH -save_path $TOKENIZED_PATH
+#python preprocess.py -mode format_to_lines -raw_path $TOKENIZED_PATH -save_path $JSON_PATH -n_cpus 64 -use_bert_basic_tokenizer false
+#python preprocess.py -mode format_to_bert -raw_path $JSON_PATH -save_path $BERT_DATA_PATH  -lower -n_cpus 64 -log_file ../logs/preprocess.log
 
