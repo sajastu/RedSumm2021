801d05a986c96f6efa2da71c9f409f21e3448d6c
sotudehg <sotudehg>
2021-08-03 20:38:19 -0400

    init

--- src/prep.sh
+++ src/prep.sh
@@ -1,10 +1,10 @@
 
 
 export RAW_PATH=/home/code-base/tldr_dataset/splits/
-export TOKENIZED_PATH=/home/code-base/large_tldr_bert/tokenized-all/
+export TOKENIZED_PATH=/home/code-base/large_tldr_bert/tokenized-all-new/
 export JSON_PATH=/home/code-base/large_tldr_bert/jsons/
 export BERT_DATA_PATH=/home/code-base/large_tldr_bert/bert-data/
-export MODEL_PATH=/home/code-base/user_space/saved_models/bertsum/presum-reddit-1024
+#export MODEL_PATH=/home/code-base/user_space/saved_models/bertsum/presum-large-reddit-1024
 
 # PREPARING DATA
 python preprocess.py -mode tokenize -raw_path $RAW_PATH -save_path $TOKENIZED_PATH
