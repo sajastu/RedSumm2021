25ae17e92a28b73f62d6fbcbbcd81cb742ae65bd
sotudehg <sotudehg>
2021-08-04 02:31:29 -0400

    init

--- src/prep.sh
+++ src/prep.sh
@@ -9,8 +9,8 @@ export BERT_DATA_PATH=$DS_BASE_DIR/bert-data/
 #export MODEL_PATH=/home/code-base/user_space/saved_models/bertsum/presum-large-reddit-1024
 
 # PREPARING DATA
-#python preprocess.py -mode tokenize -raw_path $RAW_PATH -save_path $TOKENIZED_PATH
-#python preprocess.py -mode format_to_lines -raw_path $TOKENIZED_PATH -save_path $JSON_PATH -n_cpus 60 -use_bert_basic_tokenizer false
+python preprocess.py -mode tokenize -raw_path $RAW_PATH -save_path $TOKENIZED_PATH
+python preprocess.py -mode format_to_lines -raw_path $TOKENIZED_PATH -save_path $JSON_PATH -n_cpus 60 -use_bert_basic_tokenizer false
 python preprocess.py -mode format_to_bert -raw_path $JSON_PATH -save_path $BERT_DATA_PATH  -lower -n_cpus 60 -log_file ../logs/preprocess.log
 
 
