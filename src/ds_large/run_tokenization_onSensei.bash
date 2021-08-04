41b6082de1c4bc580da160f330bb52605b467b89
sotudehg <sotudehg>
2021-08-04 15:38:54 -0400

    init

--- src/ds_large/run_tokenization_onSensei.bash
+++ src/ds_large/run_tokenization_onSensei.bash
@@ -22,28 +22,29 @@ cd $current
 
 
 
-wget -O stanford-corenlp-4.2.2.zip https://nlp.stanford.edu/software/stanford-corenlp-4.2.2.zip
-unzip stanford-corenlp-4.2.2.zip
-mkdir -p /home/code-base/toolkits/
-mv stanford-corenlp-4.2.2 /home/code-base/toolkits
-export CLASSPATH=/home/code-base/toolkits/stanford-corenlp-4.2.2/stanford-corenlp-4.2.2.jar
-source /root/.bashrc
-
-
-# now preprocessing
-mkdir -p /home/code-base/lrg_split_s/$id/splits
-mkdir -p /home/code-base/lrg_split_s/$id/tokenized
-mkdir -p /home/code-base/lrg_split_s/$id/jsons
-mkdir -p /home/code-base/lrg_split_s/$id/bert-data
-
-DS_BASE_DIR=/home/code-base/lrg_split_s/$id/
-export RAW_PATH=$DS_BASE_DIR/splits/
-export TOKENIZED_PATH=$DS_BASE_DIR/tokenized/
-export JSON_PATH=$DS_BASE_DIR/jsons/
-export BERT_DATA_PATH=$DS_BASE_DIR/bert-data/
+#wget -O stanford-corenlp-4.2.2.zip https://nlp.stanford.edu/software/stanford-corenlp-4.2.2.zip
+#unzip stanford-corenlp-4.2.2.zip
+#mkdir -p /home/code-base/toolkits/
+#mv stanford-corenlp-4.2.2 /home/code-base/toolkits
+#export CLASSPATH=/home/code-base/toolkits/stanford-corenlp-4.2.2/stanford-corenlp-4.2.2.jar
+#source /root/.bashrc
+#
+#
+## now preprocessing
+#mkdir -p /home/code-base/lrg_split_s/$id/splits
+#mkdir -p /home/code-base/lrg_split_s/$id/tokenized
+#mkdir -p /home/code-base/lrg_split_s/$id/jsons
+#mkdir -p /home/code-base/lrg_split_s/$id/bert-data
+#
+#
+#DS_BASE_DIR=/home/code-base/lrg_split_s/$id/
+#export RAW_PATH=$DS_BASE_DIR/splits/
+#export TOKENIZED_PATH=$DS_BASE_DIR/tokenized/
+#export JSON_PATH=$DS_BASE_DIR/jsons/
+#export BERT_DATA_PATH=$DS_BASE_DIR/bert-data/
 
 # PREPARING DATA
-python preprocess.py -mode tokenize -raw_path $RAW_PATH -save_path $TOKENIZED_PATH
-python preprocess.py -mode format_to_lines -raw_path $TOKENIZED_PATH -save_path $JSON_PATH -n_cpus 64 -use_bert_basic_tokenizer false
-python preprocess.py -mode format_to_bert -raw_path $JSON_PATH -save_path $BERT_DATA_PATH  -lower -n_cpus 64 -log_file ../logs/preprocess.log
+python ../preprocess.py -mode tokenize -raw_path $RAW_PATH -save_path $TOKENIZED_PATH
+python ../preprocess.py -mode format_to_lines -raw_path $TOKENIZED_PATH -save_path $JSON_PATH -n_cpus 64 -use_bert_basic_tokenizer false
+python ../preprocess.py -mode format_to_bert -raw_path $JSON_PATH -save_path $BERT_DATA_PATH  -lower -n_cpus 64 -log_file ../logs/preprocess.log
 
