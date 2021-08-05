48c621ad4b283fb50c2e86a831b5b30ced2a0b83
sotudehg <sotudehg>
2021-08-05 00:01:47 -0400

    init

--- src/ds_large/run_tokenization_onSensei.bash
+++ src/ds_large/run_tokenization_onSensei.bash
@@ -3,36 +3,49 @@
 #pip3 install spacy
 #python3 -m spacy download en_core_web_sm
 
+id=m_1
+
 
 #
 ## m0
 #file_id=1Xzr3ZUbWLTcxFPUdTAsKwsL_T0PHLSHk #my_machine
 #
 ## m1
-#file_id=18cSS5U1CxyvlaTGeKupZaRXg8Ud9RLuL #Franck -bart
+file_id=18cSS5U1CxyvlaTGeKupZaRXg8Ud9RLuL #Franck -bart
 
 ## m2
 #file_id=1oRbSyy-aZ3aJ2eVW3KLZLbBfuaCuIm6v #my_machine
 
 ## m3
-file_id=1ezy40ErwlkFNXlMx38mHItiFqynP6nbd #my_machine
+#file_id=1ezy40ErwlkFNXlMx38mHItiFqynP6nbd #my_machine
 
 ## m4
 ##file_id=1Xzr3ZUbWLTcxFPUdTAsKwsL_T0PHLSHk #my_machine
 
 ##
 
-id=m_3
-DS_BASE_DIR=/home/code-base/lrg_split_s/
+DS_BASE_DIR=/home/code-base/lrg_split_machines/
 #mkdir -p $DS_BASE_DIR
 
 ################ download from Google drive
-pip install gdown
+
+if python -c 'import pkgutil; exit(not pkgutil.find_loader("gdown"))'; then
+    echo 'gdown found'
+else
+    echo 'gdown not found... installing'
+    pip install gdown
+fi
+
 current=pwd
 cd $DS_BASE_DIR
-gdown https://drive.google.com/uc?id=$file_id -O $DS_BASE_DIR/
-#wget --no-check-certificate 'https://drive.google.com/file/d/1oRbSyy-aZ3aJ2eVW3KLZLbBfuaCuIm6v/view?usp=sharing' -O $DS_BASE_DIR/$id.tar
-echo "uncompressing"
+
+if ! [ -f $file_id.tar ]
+then
+  gdown --id $file_id
+fi
+
+
+echo "Uncompressing $id"
 tar -xf $id.tar
 cd $current
 
@@ -40,36 +53,47 @@ cd $current
 
 ################### Set up Stanford coreNlp
 #
-#wget -O stanford-corenlp-4.2.2.zip https://nlp.stanford.edu/software/stanford-corenlp-4.2.2.zip
-#unzip stanford-corenlp-4.2.2.zip
-#mkdir -p /home/code-base/toolkits/
-#mv stanford-corenlp-4.2.2 /home/code-base/toolkits
-#export CLASSPATH=/home/code-base/toolkits/stanford-corenlp-4.2.2/stanford-corenlp-4.2.2.jar
-#
-#
-#
-#
+
+if ! [ -f stanford-corenlp-4.2.2.zip ]
+then
+  wget -O stanford-corenlp-4.2.2.zip https://nlp.stanford.edu/software/stanford-corenlp-4.2.2.zip
+fi
+
+unzip stanford-corenlp-4.2.2.zip
+rm stanford-corenlp-4.2.2.zip
+
+if [ -d /home/code-base/toolkits/stanford-corenlp-4.2.2/ ]
+then
+  rm -rf /home/code-base/toolkits/stanford-corenlp-4.2.2/
+fi
+
+
+mkdir -p /home/code-base/toolkits/
+mv stanford-corenlp-4.2.2 /home/code-base/toolkits
+export CLASSPATH=/home/code-base/toolkits/stanford-corenlp-4.2.2/stanford-corenlp-4.2.2.jar
+
+
+
+
 ##################### Now preprocessing
-#
-#mkdir -p /tmp/RedSumm2021/src/logs/
-#
-##mkdir -p $DS_BASE_DIR/splits
-#mkdir -p $DS_BASE_DIR/tokenized
-#mkdir -p $DS_BASE_DIR/jsons
-#mkdir -p $DS_BASE_DIR/bert-data
-##
-#
-#export RAW_PATH=$DS_BASE_DIR/$id/
-#export TOKENIZED_PATH=$DS_BASE_DIR/tokenized/
-#export JSON_PATH=$DS_BASE_DIR/jsons/
-#export BERT_DATA_PATH=$DS_BASE_DIR/bert-data/
-#
-## PREPARING DATA
-#python /tmp/RedSumm2021/src/preprocess.py -mode tokenize -raw_path $RAW_PATH -save_path $TOKENIZED_PATH
-#python /tmp/RedSumm2021/src/preprocess.py -mode format_to_lines -raw_path $TOKENIZED_PATH -save_path $JSON_PATH -n_cpus 64 -use_bert_basic_tokenizer false
-#python /tmp/RedSumm2021/src/preprocess.py -mode format_to_bert -raw_path $JSON_PATH -save_path $BERT_DATA_PATH  -lower -n_cpus 64 -log_file ../logs/preprocess.log
-#
-#
-#echo "Compressing bert-files"
-#tar -cf $id_bertfiles.tar $BERT_DATA_PATH
-#
+
+mkdir -p /tmp/RedSumm2021/src/logs/
+mkdir -p $DS_BASE_DIR/tokenized
+mkdir -p $DS_BASE_DIR/jsons
+mkdir -p $DS_BASE_DIR/bert-data
+
+
+export RAW_PATH=$DS_BASE_DIR/$id/
+export TOKENIZED_PATH=$DS_BASE_DIR/tokenized/
+export JSON_PATH=$DS_BASE_DIR/jsons/
+export BERT_DATA_PATH=$DS_BASE_DIR/bert-data/
+
+### PREPARING DATA
+python /tmp/RedSumm2021/src/preprocess.py -mode tokenize -raw_path $RAW_PATH -save_path $TOKENIZED_PATH
+python /tmp/RedSumm2021/src/preprocess.py -mode format_to_lines -raw_path $TOKENIZED_PATH -save_path $JSON_PATH -n_cpus 64 -use_bert_basic_tokenizer false
+python /tmp/RedSumm2021/src/preprocess.py -mode format_to_bert -raw_path $JSON_PATH -save_path $BERT_DATA_PATH  -lower -n_cpus 64 -log_file ../logs/preprocess.log
+
+
+echo "Compressing bert-files"
+tar -cf $id_bertfiles.tar $BERT_DATA_PATH
+
