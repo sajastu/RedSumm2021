f2d64991c330fa6a85fe492ce9660a64a8a54aeb
sotudehg <sotudehg>
2021-08-05 16:18:42 -0400

    init

--- src/ds_large/run_tokenization_onSensei.bash
+++ src/ds_large/run_tokenization_onSensei.bash
@@ -1,19 +1,21 @@
 
 #################  CONFIGS  #############################
-export id=m_1
-
-mkdir -p /tmp/RedSumm2021/src/logs/
-mkdir -p $DS_BASE_DIR
-mkdir -p $DS_BASE_DIR/tokenized
-mkdir -p $DS_BASE_DIR/jsons
-mkdir -p $DS_BASE_DIR/bert-data
+export id=m_4
 
 export DS_BASE_DIR=/home/code-base/lrg_split_machines/
-#export RAW_PATH=/home/code-base/tldr_dataset/machine_splits/$id
 export RAW_PATH=$DS_BASE_DIR/$id/
 export TOKENIZED_PATH=$DS_BASE_DIR/tokenized/
 export JSON_PATH=$DS_BASE_DIR/jsons/
 export BERT_DATA_PATH=$DS_BASE_DIR/bert-data-$id/
+
+mkdir -p /tmp/RedSumm2021/src/logs/
+mkdir -p $DS_BASE_DIR
+mkdir -p $TOKENIZED_PATH
+mkdir -p $JSON_PATH
+mkdir -p $BERT_DATA_PATH
+
+#export RAW_PATH=/home/code-base/tldr_dataset/machine_splits/$id
+
 
 
 ### file ids to be downloaded
@@ -92,8 +94,8 @@ export CLASSPATH=/home/code-base/toolkits/stanford-corenlp-4.2.2/stanford-corenl
 
 #
 #### PREPARING DATA
-python /tmp/RedSumm2021/src/preprocess.py -mode tokenize -raw_path $RAW_PATH -save_path $TOKENIZED_PATH
-python /tmp/RedSumm2021/src/preprocess.py -mode format_to_lines -raw_path $TOKENIZED_PATH -save_path $JSON_PATH -n_cpus 64 -use_bert_basic_tokenizer false
+#python /tmp/RedSumm2021/src/preprocess.py -mode tokenize -raw_path $RAW_PATH -save_path $TOKENIZED_PATH
+#python /tmp/RedSumm2021/src/preprocess.py -mode format_to_lines -raw_path $TOKENIZED_PATH -save_path $JSON_PATH -n_cpus 64 -use_bert_basic_tokenizer false
 python /tmp/RedSumm2021/src/preprocess.py -mode format_to_bert -raw_path $JSON_PATH -save_path $BERT_DATA_PATH  -lower -n_cpus 64 -log_file ../logs/preprocess.log
 
 cd $DS_BASE_DIR
