e9a782cfaa0a3a8d26e9dbbbdb18cf3d9051c8a2
sotudehg <sotudehg>
2021-08-05 20:00:52 -0400

    init

--- src/ds_large/donwload_bertFiles_and_integrate.sh
+++ src/ds_large/donwload_bertFiles_and_integrate.sh
@@ -9,10 +9,10 @@ mkdir -p $STORE_DIR
 URL_m0=https://drive.google.com/file/d/17t1ADPuRdiyDUdTULDtRj59vpviVgAOO/edit
 
 # m1
+URL_m1=https://drive.google.com/file/d/19tenZFat7Sj1_gLaHg1iKf7_Ng_L2Jmj/edit
 
-# m2
-
-# m3
+# m2, m3
+URL_m3=https://drive.google.com/file/d/1-PuqDptfdrCQAXSetJkKK-Oj8cj27Ri3/edit
 
 # m4
 URL_m4=https://drive.google.com/file/d/1MZEqFf0_77TDzdWE3PgFnKOzjSeeGXl-/edit
@@ -26,9 +26,7 @@ perl gdown.pl/gdown.pl  $STORE_DIR/m4-bertFiles.tar
 
 ################## Uncompressing bertfiles, should note that it should not interfere names
 
-tar -xf $STORE_DIR/m0-bertFiles.tar
-tar -xf $STORE_DIR/m1-bertFiles.tar
-tar -xf $STORE_DIR/m2-bertFiles.tar
-tar -xf $STORE_DIR/m3-bertFiles.tar
-
-#python united_bert_files.py $STORE_DIR
+tar -xf $STORE_DIR/m0-bertFiles.tar --directory $STORE_DIR/
+tar -xf $STORE_DIR/m1-bertFiles.tar --directory $STORE_DIR/
+tar -xf $STORE_DIR/m2-bertFiles.tar --directory $STORE_DIR/
+tar -xf $STORE_DIR/m3-bertFiles.tar --directory $STORE_DIR/
