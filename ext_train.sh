9d1d49159d259d2ef1107f68a34c9b20a1f95df7
sotudehg <sotudehg>
2021-08-06 12:19:02 -0400

    init

--- ext_train.sh
+++ ext_train.sh
@@ -9,6 +9,7 @@ mkdir -p logs
 
 python src/train.py -task ext -mode train \
                           -bert_data_path $BERT_DATA_PATH -ext_dropout 0.1 -model_path $MODEL_PATH \
-                          -lr 2e-3 -visible_gpus 0,1,2,3,4,5,6,7 -report_every 50 -save_checkpoint_steps 1000 \
+                          -lr 2e-3 -visible_gpus 0 -report_every 50 -save_checkpoint_steps 1000 \
                           -batch_size 34000 -train_steps 1000000 -accum_count 2 \
                           -log_file logs/ext_bert_reddit.log -use_interval true -warmup_steps 20000 -max_pos 1024
+#0,1,2,3,4,5,6,7
