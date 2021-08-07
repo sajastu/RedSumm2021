bd14fa408aea12d3ec9dfeb300a9cea413678e43
sotudehg <sotudehg>
2021-08-07 16:05:56 -0400

    init

--- ext_train.sh
+++ ext_train.sh
@@ -12,5 +12,5 @@ python src/train.py -task ext -mode train \
                           -lr 2e-3 -visible_gpus 0,1,2,3,4,5,6,7 -report_every 100 -save_checkpoint_steps 50000 \
                           -batch_size 5000 -test_batch_size 10000 -train_steps 1500000 -accum_count 2 \
                           -log_file logs/ext_bert_reddit.log -use_interval true -warmup_steps 20000 -max_pos 1024 \
-                          -train_from /home/code-base/user_space/saved_models/bertsum/bertExt-reddit-1024/
+                          -train_from /home/code-base/user_space/saved_models/bertsum/bertExt-reddit-1024/model_step_50000.pt
 #0,1,2,3,4,5,6,7
