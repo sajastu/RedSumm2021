87c1ec89e837b3598cd4267bb93a523d23db6544
sotudehg <sotudehg>
2021-08-20 17:11:55 -0400

    init

--- src/prepro/data_builder.py
+++ src/prepro/data_builder.py
@@ -613,8 +613,8 @@ def move_subset(args):
         for f in tqdm(files, total=len(files), desc=f'{split}'):
             try:
 
-                shutil.copy(args.raw_path + f, args.save_path)
                 import pdb;pdb.set_trace()
+                shutil.copy(args.raw_path + f, args.save_path)
                 os.rename(args.save_path + f, args.save_path + split + '-' + '-'.join(f.split('-')[1:]))
             except:
                 continue
